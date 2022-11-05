b4c30dda4f5a23b649f2e5a7ea7c9d0d31c462f6
Rafael <faelmota7@gmail.com>
2022-11-04 21:55:41 -0300

    Resolução

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿Console.Write("Números pares entre 0 e ? ");
+int  i = Convert.ToInt32(Console.ReadLine());
+
+if (i >=1)
+{
+   int numeroPar = 0;
+
+    while (numeroPar <= i) 
+        {
+            Console.ForegroundColor=ConsoleColor.DarkGreen;
+            
+            Console.Write($"{numeroPar} ");
+            numeroPar = numeroPar + 2;
+
+            Console.ResetColor();
+        }
+
+        Console.WriteLine();
+}
+
+else
+{
+    Console.ForegroundColor=ConsoleColor.DarkRed;
+    Console.WriteLine($"O número {i} não é positivo");
+    Console.ResetColor();
+}
+
